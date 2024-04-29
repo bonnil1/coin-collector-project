67bf6b11ff6fbb26e026cc230e711d6852abdc7f
bonnil1 <bonnieliu759@yahoo.com>
2024-04-29 12:56:27 -0700

    tailwind css works

--- coincollector_project/main_app/views.py
+++ coincollector_project/main_app/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
+coins = [
+    {'name': 'Bitcoin', 'symbol': 'BTC', 'price': '63,332.98'},
+    {'name': 'Ethereum', 'symbol': 'ETH', 'price': '3,247,03'},
+    {'name': 'Dogecoin', 'symbol': 'DOGE', 'price': '0.1475'},
+]
+
 # Create your views here.
+def home(request):
+    return render(request, 'coins/home.html')
+
+def about(request):
+    return render(request, 'coins/about.html')
+
+def coins_index(request):
+    return render(request, 'coins/index.html', {
+        'coins': coins
+    })
